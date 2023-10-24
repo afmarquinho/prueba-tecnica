6fd938e25fd440991b1c4a8adfee25f12b612777
afmarquinhos <fernandez9029@gmail.com>
2023-10-24 16:57:04 -0500

    deployment1

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.exports={
-   publicPath: "/prueba-tecnica/" 
-}
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/prueba-tecnica/'  // Cambia esto con el nombre de tu repositorio
+      : '/',
+  }
