d3befa35c8985c0b56b0dc885e3cd24374630eac
afmarquinhos <fernandez9029@gmail.com>
2023-10-24 17:12:18 -0500

    conf de vite

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://afmarquihno.github.io/prueb-tecnica",
   plugins: [
     vue(),
     AutoImport({
